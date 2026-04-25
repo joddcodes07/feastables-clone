2da3b7a3a9a662ecd223c29e0e678aefbdb055cc
Yashpal Singh <yashsinghh0777@gmail.com>
2026-04-25 23:53:18 +0530

    Top image update

---
+++ src/Components/OurStory/StoryTopImage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const StoryTopImage = () => {
+  return (
+    <section className="w-full border-b-4 border-black bg-black">
+      <img 
+        src="https://feastables.com/cdn/shop/files/aboutus-hero-web.jpg?v=1751051983&width=3200" 
+        alt="Feastables Team" 
+        className="w-full h-auto object-cover object-center"
+      />
+    </section>
+  );
+};
+
+export default StoryTopImage;
